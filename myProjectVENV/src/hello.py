2d1034323b4b795c6c90bb320d136cc1268f697c
周启航 <qh.zhou@siat.ac.cn>
2015-06-24 14:21:37 +0800

    add venv
    在myProjectVENV文件夹下添加了venv环境

--- myProjectVENV/src/hello.py
+++ myProjectVENV/src/hello.py
@@ -10,11 +10,20 @@ def printMyName():
 
 @app.route('/user/<username>')
 def show_user_profile(username):
-	return 'User $s' % username
+	return 'User %s' % username
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
 	return 'Post_ID %d' % post_id
+
+@app.route('/projects/')
+def projects():
+	return 'Projects Hello!'
+
+@app.route('/about')
+def about():
+	return 'About Hello!'
+
 
 if __name__=='__main__':
 	app.debug=True
