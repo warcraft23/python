84d6786db2719271a9e8ca71109611c7e8b7314f
周启航 <qh.zhou@siat.ac.cn>
2016-05-18 13:47:49 +0800

    add sqlite3 and multithread
    使用了sqlite3作为数据库，同时具备多线程获取起始url的功能

--- MySpider/WooyunScrapyer/WooyunScrapyer/pipelines.py
+++ MySpider/WooyunScrapyer/WooyunScrapyer/pipelines.py
@@ -10,29 +10,136 @@
 # 需要做的仅仅是SQL语句的插入操作，就是insert咯，我先试试哈
 
 import mysql.connector
+import sqlite3
+import os
+import sys
+
+WooyunScrapyerWorkdir = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+DBFILENAME = os.path.join(WooyunScrapyerWorkdir, "db/WooyunBugs.db")
+
+CreateWooyunBugsTableSql = '''CREATE TABLE IF NOT EXISTS `BugDetail` (
+`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+`bugindex` varchar(1024),
+`title` varchar(1024),
+`corp` varchar(256),
+`author` varchar(256),
+`submit_date` varchar(256),
+`open_date` varchar(256),
+`type` varchar(256),
+`level` varchar(64),
+`myrank` varchar(256),
+`status` varchar(256),
+`tags` varchar(10240)
+)'''
+
+CreateWooyunBugsIndexSql = "create index BugDetail_index on BugDetail(bugindex,title,type)"
+
+sql_insert = '''INSERT INTO
+BugDetail(bugindex,title,corp,author,submit_date,open_date,type,level,myrank,status,tags)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+
+def tableExist(tbname):
+    if not os.path.exists(DBFILENAME):
+        print "table[%s] doesn't exist" %(tbname)
+        return False
+    else:
+        sqlstr = "select count(*) from sqlite_master where type='table' and name='%s'" %(tbname)
+        conn = sqlite3.connect(DBFILENAME)
+        cu = conn.cursor()
+        cu.execute(sqlstr)
+        r = cu.fetchall()
+        cu.close()
+        conn.close()
+        if r[0][0] == 1:
+            return True
+        else:
+            print "table[%s] doesn't exist" %(tbname)
+            return False
+
+def create_table(tbname, crttblsql, crtindexsql):
+    if tableExist(tbname) is False:
+        conn = sqlite3.connect(DBFILENAME)
+        cu = conn.cursor()
+        cu.execute(crttblsql)
+        conn.commit()
+        cu.execute(crtindexsql)
+        conn.commit()
+        cu.close()
+        conn.close()
+
+def excuteSelSql(conn,sql):
+    if sql is not None and sql !='':
+        cu = conn.cursor()
+        cu.execute(sql)
+        r = cu.fetchall()
+        cu.close()
+        if len(r) > 0:
+            return r
+        else:
+            return None
+
+def excutesqlstr(conn, sql):
+    if sql is not None and sql != '':
+        cu = conn.cursor()
+        cu.execute(sql)
+        conn.commit()
+        cu.close
+
+def excuteInsUpdDelSql(conn, sql, data):
+    if sql is not None and sql != '':
+        if data is not None:
+            cu = conn.cursor()
+            for d in data:
+                cu.execute(sql,d)
+                conn.commit()
+            cu.close()
+
 
 class WooyunscrapyerPipeline(object):
     def open_spider(self, spider):
-        self.conn = mysql.connector.connect(host='localhost', user='Edward', passwd='123', db='BugsInWooyun')
-        self.cursor = self.conn.cursor()
+        create_table("BugDetail", CreateWooyunBugsTableSql, CreateWooyunBugsIndexSql)
+        # self.conn = mysql.connector.connect(host='localhost', user='Edward', passwd='123', db='BugsInWooyun')
+        # self.cursor = self.conn.cursor()
+        self.conn = sqlite3.connect(DBFILENAME,check_same_thread=False)
+        self.conn.text_factory = str
+        # self.cursor = self.conn.cursor()
 
     def close_spider(self, spider):
-        self.cursor.close()
         self.conn.close()
+        # self.conn.close()
 
     def process_item(self, item, spider):
-        sql_insert = 'INSERT INTO BugDetail(bug_id,title,corp,author,submit_date,open_date,type,level,myrank,status,tags) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
         tags = ''
         if item['wybug_tags']:
             for str in item['wybug_tags']:
                 tags += str + ' '
         else:
             tags = '无'
-        data = (item['wybug_index'][0], item['wybug_title'][0], item['wybug_corp'][0], item['wybug_author'][0], item['wybug_submit_date'][0], item['wybug_open_date'][0], item['wybug_type'][0], item['wybug_level'][0], item['wybug_myrank'][0], item['wybug_status'][0], tags)
-        try:
-            self.cursor.execute(sql_insert, data)
-            self.conn.commit()
-        except:
-            pass
+        data = (item['wybug_index'][0], item['wybug_title'][0], item['wybug_corp'][0],
+                item['wybug_author'][0], item['wybug_submit_date'][0], item['wybug_open_date'][0], item['wybug_type'][0],
+                item['wybug_level'][0], item['wybug_myrank'][0], item['wybug_status'][0], tags)
+        newdata = []
+        newdata.append(data)
+        print "======================================"
+        # print newdata[0][0]
+        checkstr = "select bugindex,title,corp,author,submit_date,open_date," \
+                   "type,level,myrank,status,tags from BugDetail where bugindex='%s'" % (newdata[0][0])
+        # print checkstr
+        olddata = excuteSelSql(self.conn, checkstr)
+        if olddata is None:
+            excuteInsUpdDelSql(self.conn, sql_insert, newdata)
+        else:
+            if cmp(olddata, newdata) != 0:
+                UpdateBugInfoSql = "update BugDetail " \
+                                   "set title='%s',corp='%s',author='%s',submit_date='%s',open_date='%s'," \
+                                   "type='%s',level='%s',myrank='%s',status='%s',tags='%s' where bugindex='%s'" % (
+                    newdata[0][1], newdata[0][2], newdata[0][3], newdata[0][4], newdata[0][5], newdata[0][6],
+                    newdata[0][7], newdata[0][8], newdata[0][9], newdata[0][10], newdata[0][0]
+                )
+                excutesqlstr(self.conn, UpdateBugInfoSql)
 
         return item
--- MySpider/WooyunScrapyer/WooyunScrapyer/spiders/wooyunScrapyer.py
+++ MySpider/WooyunScrapyer/WooyunScrapyer/spiders/wooyunScrapyer.py
@@ -11,58 +11,31 @@ import sys
 sys.path.append('..')
 reload(sys)
 sys.setdefaultencoding("utf-8")
-
 from WooyunScrapyer.items import Bug
-
-
 
 class WooyunscrapyerSpider(scrapy.Spider):
     name = "wooyunScrapyer"
     allowed_domains = ["http://www.wooyun.org"]
     start_urls = []
 
+    def __init__(self, roundnum=None,*args,**kwargs):
+        self.roundnum = int(roundnum)
+        super(WooyunscrapyerSpider, self).__init__(*args, **kwargs)
+
     def start_requests(self):
-        index = 538
-        # while True:
-        #     if index == 3:
-        #         break
-        #     print 'Crawling Page', index, 'OF Public Bugs in 2015'
-        #     print "======================================"
-        #     url_base = "http://www.wooyun.org/bugs/new_public/page/"
-        #     url = url_base + str(index)
-        #     index += 1
-        #     request = urllib2.Request(url)
-        #
-        #     try:
-        #         response = urllib2.urlopen(request)
-        #     except urllib2.HTTPError, e:
-        #         print e.code
-        #     except urllib2.URLError, e:
-        #         print e.reason
-        #
-        #     # 读取其中缺陷编号为2015的漏洞
-        #     pattern = re.compile(r'a href="/bugs/wooyun-2015-(\d+)"')
-        #     result = re.findall(pattern, response.read())
-        #     if result:
-        #         for item in result:
-        #             bugURL = "http://www.wooyun.org/bugs/wooyun-2015-" + item
-        #             print "Insert URL:", bugURL, "into start_urls"
-        #             self.start_urls.append(bugURL)
-        #             print "======================================"
-        #     else:
-        #         # 如果当前页面不存在编号2015带头的漏洞，则结束抓取链接，进入抓取漏洞详情函数
-        #         break
+        threadpool = []
+        for i in range(self.roundnum):
+            start_index = 1+i*10
+            end_index = (i+1)*10
+            th = threading.Thread(target=self.get_bug_URL, args=(start_index, end_index))
+            threadpool.append(th)
 
-        # cluster = 27
-        # for cnt in xrange(cluster):
-        #     threadBugURL = threading.Thread(name="BugURLThread"+str(cnt), target=self.get_bug_URL, args=(index+20*(cnt-1), index+20*cnt))
-        #     # threadBugURL.setDaemon(True)
-        #     threadBugURL.start()
+        for th in threadpool:
+            th.start()
+        for th in threadpool:
+            threading.Thread.join(th)
 
-
-        self.get_bug_URL(index, 539)
-
-        print self.start_urls
+        # print self.start_urls
         start = ''
         while start is not 'y':
             print 'Shall We Start? y/n'
@@ -78,12 +51,14 @@ class WooyunscrapyerSpider(scrapy.Spider):
         while True:
             if index == end:
                 break
-            print 'Crawling Page', index, 'OF Public Bugs in 2015'
-            print "======================================"
+            # print 'Crawling Page', index, 'OF Public Bugs'
+            # print "======================================"
             url_base = "http://www.wooyun.org/bugs/new_public/page/"
             url = url_base + str(index)
             index += 1
-            request = urllib2.Request(url)
+            request = urllib2.Request(url, headers={
+                                          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.86 Safari/537.36"
+                                      })
 
             try:
                 response = urllib2.urlopen(request)
@@ -92,15 +67,15 @@ class WooyunscrapyerSpider(scrapy.Spider):
             except urllib2.URLError, e:
                 print e.reason
 
-            # 读取其中缺陷编号为2015的漏洞
-            pattern = re.compile(r'a href="/bugs/wooyun-2015-(\d+)"')
+            # 读取其中漏洞
+            pattern =re.compile(r'a href="/bugs/wooyun-(201\d-\d+)"')
             result = re.findall(pattern, response.read())
             if result:
                 for item in result:
-                    bugURL = "http://www.wooyun.org/bugs/wooyun-2015-" + item
-                    print "Insert URL:", bugURL, "into start_urls"
+                    bugURL = "http://www.wooyun.org/bugs/wooyun-" + item
+                    # print "Insert URL:", bugURL, "into start_urls"
                     self.start_urls.append(bugURL)
-                    print "======================================"
+                    # print "======================================"
             else:
                 # 如果当前页面不存在编号2015带头的漏洞，则结束抓取链接，进入抓取漏洞详情函数
                 break
@@ -111,8 +86,8 @@ class WooyunscrapyerSpider(scrapy.Spider):
 
         #设置漏洞编号
         bug['wybug_index']      = sel.xpath('//div[@class="content"]/h3[1]/a/text()').extract()
-        for item in bug['wybug_index']:
-            print '[', item, ']'
+        # for item in bug['wybug_index']:
+        #     print '[', item, ']'
 
         #设置漏洞标题
         title                   = sel.xpath('//div[@class="content"]/h3[2]/text()').extract()[0]
@@ -120,20 +95,20 @@ class WooyunscrapyerSpider(scrapy.Spider):
         title                   = re.sub(r'[\s\r\n]', '', title)
         title                   = re.sub(r'漏洞标题：', '', title)
         bug['wybug_title']      = [title.decode('utf-8')]
-        for item in bug['wybug_title']:
-            print item
+        # for item in bug['wybug_title']:
+        #     print item
 
         #设置漏洞厂商
         corp                    = sel.xpath('//div[@class="content"]/h3[3]/a/text()').extract()[0]
         corp                    = re.sub(r'[\s\r\n]', '', corp)
         bug['wybug_corp']       = [corp]
-        for item in bug['wybug_corp']:
-            print item
+        # for item in bug['wybug_corp']:
+        #     print item
 
         #设置漏洞作者
         bug['wybug_author']     = sel.xpath('//div[@class="content"]/h3[4]/a/text()').extract()
-        for item in bug['wybug_author']:
-            print item
+        # for item in bug['wybug_author']:
+        #     print item
 
         #设置漏洞提交时间
         time                    = sel.xpath('//div[@class="content"]/h3[@class="wybug_date"]/text()').extract()[0]
@@ -141,8 +116,8 @@ class WooyunscrapyerSpider(scrapy.Spider):
         time                    = re.sub(r'[\r\n\t]', '', time)
         time                    = re.sub(r'提交时间：', '', time)
         bug['wybug_submit_date']= [time.decode('utf-8')]
-        for item in bug['wybug_submit_date']:
-            print item
+        # for item in bug['wybug_submit_date']:
+        #     print item
 
         #设置漏洞公开时间
         time                    = sel.xpath('//div[@class="content"]/h3[@class="wybug_open_date"]/text()').extract()[0]
@@ -150,8 +125,8 @@ class WooyunscrapyerSpider(scrapy.Spider):
         time                    = re.sub(r'[\r\n\t]', '', time)
         time                    = re.sub(r'公开时间：', '', time)
         bug['wybug_open_date']  = [time.decode('utf-8')]
-        for item in bug['wybug_open_date']:
-            print item
+        # # for item in bug['wybug_open_date']:
+        #     print item
 
         #设置漏洞类型
         type                    = sel.xpath('//div[@class="content"]/h3[@class="wybug_type"]/text()').extract()[0]
@@ -159,8 +134,8 @@ class WooyunscrapyerSpider(scrapy.Spider):
         type                    = re.sub(r'[\t\r\s\n]', '', type)
         type                    = re.sub(r'漏洞类型：', '', type)
         bug['wybug_type']  = [type.decode('utf-8')]
-        for item in bug['wybug_type']:
-            print item
+        # for item in bug['wybug_type']:
+        #     print item
 
         #设置危害等级
         level                   = sel.xpath('//div[@class="content"]/h3[@class="wybug_level"]/text()').extract()[0]
@@ -168,8 +143,8 @@ class WooyunscrapyerSpider(scrapy.Spider):
         level                   = re.sub(r'[\t\r\s\n]', '', level)
         level                   = re.sub(r'危害等级：', '', level)
         bug['wybug_level']      = [level.decode('utf-8')]
-        for item in bug['wybug_level']:
-            print item
+        # for item in bug['wybug_level']:
+        #     print item
 
         #设置RANK
         myPattern               = re.compile(r'自评Rank：([\t\d]+)')
@@ -177,8 +152,8 @@ class WooyunscrapyerSpider(scrapy.Spider):
         rank                    = re.sub(r'[\t\r\s\n]', '', rank)
         rank                    = re.sub(r'自评Rank：', '', rank)
         bug['wybug_myrank']     = [int(rank)]
-        for item in bug['wybug_myrank']:
-            print item
+        # for item in bug['wybug_myrank']:
+        #     print item
 
         #设置漏洞状态
         status                  = sel.xpath('//div[@class="content"]/h3[@class="wybug_status"]/text()').extract()[0]
@@ -186,14 +161,14 @@ class WooyunscrapyerSpider(scrapy.Spider):
         status                  = re.sub(r'[\t\r\s\n]', '', status)
         status                  = re.sub(r'漏洞状态：', '', status)
         bug['wybug_status']     = [status.decode('utf-8')]
-        for item in bug['wybug_status']:
-            print item
+        # for item in bug['wybug_status']:
+        #     print item
 
         #设置漏洞标签
         bug['wybug_tags']       = sel.xpath('//span[@class="tag"]/a/text()').extract()
-        for item in bug['wybug_tags']:
-            print item
+        # for item in bug['wybug_tags']:
+        #     print item
 
-        print '==================================================='
+        # print '==================================================='
 
         return bug
